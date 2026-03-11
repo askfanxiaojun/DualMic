8a05be664ea6264108f5354124cc60fd956b7a34
隔壁樊同学 <askfanxiaojun@163.com>
2026-03-11 14:51:46 +0800

    Initial Commit

---
+++ DualMic/DualMicApp.swift
@@ -0,0 +1,17 @@
+//
+//  DualMicApp.swift
+//  DualMic
+//
+//  Created by 樊笑君 on 3/11/26.
+//
+
+import SwiftUI
+
+@main
+struct DualMicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
